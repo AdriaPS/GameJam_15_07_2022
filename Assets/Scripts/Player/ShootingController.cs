5527b25703133468f98050d930e8537f0d097143
Alex Ruiz <aruizr@edu.tecnocampus.cat>
2022-07-16 00:30:26 +0200

    Player prefab

--- Assets/Scripts/Player/ShootingController.cs
+++ Assets/Scripts/Player/ShootingController.cs
@@ -1,7 +1,39 @@
-﻿namespace Player
+﻿using Codetox.Misc;
+using Codetox.Pooling;
+using Codetox.Variables;
+using UnityEngine;
+
+namespace Player
 {
-    public class ShootingController
+    public class ShootingController: MonoBehaviour
     {
+        [SerializeField] private Transform playerTransform;
+        [SerializeField] private ValueReference<Vector2> direction;
+        [SerializeField] private ValueReference<float> bulletsPerSecond;
+        [SerializeField] private GameObjectPool bulletPool;
+
+        private Range<float> xRange = new(-0.75f, 0.75f);
+
+        public void Shoot()
+        {
+            if (IsPointingUp())
+            {
+                ShootBullet(playerTransform.up);
+            }
+            else
+            {
+                ShootBullet(playerTransform.forward);
+            }
+        }
         
+        private void ShootBullet(Vector3 dir)
+        {
+            var bullet = bulletPool.Get();
+        }
+
+        private bool IsPointingUp()
+        {
+            return direction.Value.y > 0f && xRange.IsInRange(direction.Value.x);
+        }
     }
 }
