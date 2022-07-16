909a3a4f263faa8c36ce666d4db97ea84cd80d48
Shadowini <adriapujolsierra@gmail.com>
2022-07-17 01:00:17 +0200

    🐸🐸🐸🐸🐸🐸

--- Assets/Scripts/Managers/WaveManager.cs
+++ Assets/Scripts/Managers/WaveManager.cs
@@ -1,20 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Codetox.Variables;
 using UnityEngine;
 
+[System.Serializable]
 public class Wave
 {
     public int numEnemies;
-    public GameObject typesEnemies;
     public float waveDuration;
 }
 
 public class WaveManager : MonoBehaviour
 {
+    public Transform[] groundPoints;
+    public Transform[] skyPoints;
+    public Wave[] waves;
+    public GameObject[] typesEnemies;
+    public Variable<float> timeIncrease;
+    public Variable<int> enemiesIncrease;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        timeIncrease.Value = 20.0f;
+        enemiesIncrease.Value = 5;
     }
 
     // Update is called once per frame
---
+++ Assets/Shooter_Enemy.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Codetox.Core;
+using Codetox.Variables;
+using UnityEngine;
+
+public class Shooter_Enemy : MonoBehaviour
+{
+    public Transform shootingPoint;
+    public Animator animator;
+    public Variable<Vector2> player;
+    public GameObject bullet;
+
+    private void OnEnable()
+    {
+        if (transform.DirectionTo(player.Value).x * transform.right.x > 0)
+        {
+            transform.Rotate(0, 180, 0);
+        }
+    }
+
+    public void Triggered()
+    {
+        InvokeRepeating("Attack", 0.5f, 2);
+    }
+
+    public void Attack()
+    {
+        animator.Play("RombusAttackAnim");
+        Instantiate(bullet, shootingPoint);
+    }
+
+    public void CancelInvoke()
+    {
+        CancelInvoke("Attack");
+    }
+}
