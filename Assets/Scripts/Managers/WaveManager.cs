e744c7c38ddd6ebb885f8fe0468f120a4afcb339
Shadowini <adriapujolsierra@gmail.com>
2022-07-16 22:33:08 +0200

    😎

--- Assets/Scripts/Managers/WaveManager.cs
+++ Assets/Scripts/Managers/WaveManager.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Wave
 {
-    public int numEnemies; 
+    public int numEnemies;
+    public GameObject typesEnemies;
+    public float waveDuration;
 }
 
 public class WaveManager : MonoBehaviour
