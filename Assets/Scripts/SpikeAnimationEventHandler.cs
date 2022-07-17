2467d529832566e8f9c21e7c8a20ec578f59fc13
Alex Ruiz <aruizr@edu.tecnocampus.cat>
2022-07-17 16:09:01 +0200

    Font

---
+++ Assets/Scripts/SpikeAnimationEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class SpikeAnimationEventHandler : MonoBehaviour
+{
+    public UnityEvent onSpikeUp, onSpikeDown;
+
+    public void OnSpikeUp()
+    {
+        onSpikeUp?.Invoke();
+    }
+    
+    public void OnSpikeDown()
+    {
+        onSpikeDown?.Invoke();
+    }
+}
