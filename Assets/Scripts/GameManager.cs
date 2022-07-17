404809cb92a564f209ebb7a8b0be41024735d52e
Alex Ruiz <aruizr@edu.tecnocampus.cat>
2022-07-17 17:43:05 +0200

    Cambiar de menu a juego

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(fileName = "Game Manager", menuName = "Game Manager", order = 0)]
+public class GameManager : ScriptableObject
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    
+    public void RestartCurrentScene()
+    {
+        var scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+    
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+    
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
